ebdb31b9c7ac3e2b53fcc0b2dc4e11b41e04e37b
K3ntuwo <cabrerakenadrian106@gmail.com>
2024-11-12 18:17:03 +0800

    sicund kumit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -45,14 +45,14 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/about-image.png" width={500} height={500} alt="about image" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          I’m skilled in programming and have strong computer literacy, which helps me navigate and understand various
+          {`I’m skilled in programming and have strong computer literacy, which helps me navigate and understand various
            technologies with ease. Coding is more than just a skill for me; it's a passion. I’m always eager to take on
             new projects, learn emerging technologies, and solve complex problems. This drive keeps me motivated and 
-            constantly expanding my knowledge in the tech field.
+            constantly expanding my knowledge in the tech field.`}
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
